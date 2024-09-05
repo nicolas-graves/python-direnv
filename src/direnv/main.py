13206d1a607f65b9655e6af23c51e236b78ac205
Nicolas Graves <ngraves@ngraves.fr>
2024-09-05 10:04:50 +0200

    find_direnv: Copy full function from find_dotenv.
    Some tests were failing when relying on direnv calling dotenv. The
    issue was that the path considered was the path of direnv instead of
    the patch of the .envrc location.

--- src/direnv/main.py
+++ src/direnv/main.py
@@ -1,6 +1,6 @@
 import os
 import dotenv
-from dotenv.main import DotEnv
+from dotenv.main import DotEnv, _walk_to_root
 import subprocess
 from pathlib import Path
 import hashlib
@@ -78,12 +78,39 @@ def find_direnv(
     Returns path to the file if found, or an empty string otherwise
     """
 
-    result = dotenv.find_dotenv(filename, raise_error_if_not_found, usecwd)
-    if result != "":
-        return result
-    return dotenv.find_dotenv(
-        raise_error_if_not_found=raise_error_if_not_found, usecwd=usecwd
-    )
+    def _is_interactive():
+        """ Decide whether this is running in a REPL or IPython notebook """
+        try:
+            main = __import__('__main__', None, None, fromlist=['__file__'])
+        except ModuleNotFoundError:
+            return False
+        return not hasattr(main, '__file__')
+
+    if usecwd or _is_interactive() or getattr(sys, 'frozen', False):
+        # Should work without __file__, e.g. in REPL or IPython notebook.
+        path = os.getcwd()
+    else:
+        # will work for .py files
+        frame = sys._getframe()
+        current_file = __file__
+
+        while frame.f_code.co_filename == current_file or not os.path.exists(
+            frame.f_code.co_filename
+        ):
+            assert frame.f_back is not None
+            frame = frame.f_back
+        frame_filename = frame.f_code.co_filename
+        path = os.path.dirname(os.path.abspath(frame_filename))
+
+    for dirname in _walk_to_root(path):
+        check_path = os.path.join(dirname, filename)
+        if os.path.isfile(check_path):
+            return check_path
+
+    if raise_error_if_not_found:
+        raise IOError('File not found')
+
+    return ''
 
 
 def load_direnv(
